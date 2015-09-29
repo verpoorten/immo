d2c303c0991d145985d5535a58004f0b170be44c
Your Name <you@example.com>
2015-09-29 14:26:31 +0200

    policy holder and executor ok

--- models/contract.py
+++ models/contract.py
@@ -4,13 +4,10 @@ from openerp import models, fields
 
 class Contract(models.Model):
     _name = 'immo.contract' 
-    _inherits=[['immo.building','building_id'],['res.partner','partner_id'],]
+    _inherits=[['immo.building','building_id'],]
     
     description = fields.Char()   
     date_start = fields.Date(required=True)  
     date_end = fields.Date(required=True)
-#     policy_holder_id = fields.Many2one('res.partner', string='Policy holder', ondelete="restrict")
-#     executor_id = fields.Many2one('res.partner', string='Executor', ondelete="restrict")
-#     building_ids = fields.One2many('immo.building', 'contract_id', string="Buildings")
-#     executor_id = fields.Many2many('res.partner', relation='immo_policy_holder', column1='contract_id', column2='policy_holder_id')
-#     student_ids = fields.Many2many('res.partner', domain="[('student', '=', 1)]", relation='epc_student_activityinfo', column1='activity_id', column2='student_id')
+    policy_holder_id = fields.Many2one('res.partner',string="policy holder")
+    executor_id = fields.Many2one('res.partner',string="executor")
--- models/partner.py
+++ models/partner.py
@@ -12,8 +12,12 @@ class Partner(models.Model):
     rental_ids = fields.Many2many('immo.rental', relation='immo_tenant_rental', column1='tenant_id', column2='rental_id')
 #     contract_policy_holder_ids = fields.Many2many('immo.contract', relation='immo_policy_holder_contract', column1='policy_holder_id', column2='contract_id')
 #     contract_executor_ids = fields.Many2many('immo.contract', relation='immo_executor_contract', column1='executor_id', column2='contract_id')
-    contract_policy_holder_ids = fields.One2many('immo.contract', 'partner_id',string='contracts')
-#     contract_executor_ids = fields.One2many('immo.contract', relation='immo_executor_contract', column1='executor_id', column2='contract_id')
+#     contract_policy_holder_ids = fields.One2many('immo.contract', 'partner_id',string='contracts')
+    contract_policy_holder_ids = fields.One2many('immo.contract','policy_holder_id', string='Policy holder')
+    contract_executor_ids = fields.One2many('immo.contract','executor_id', string='Executor')
+    
+#     contract_executor_holder_ids = fields.One2many('immo.contract', 'executor_id',string='contracts')
+
     
     
    
